40ddea504c9e3b47e2bd9f75e4a655a233fa749b
Giovannacm <giovannacm21@gmail.com>
2021-05-18 16:08:36 -0300

    Finalizando a parte I

--- static/script/summary.js
+++ static/script/summary.js
@@ -1,38 +1,10 @@
 var order = {};
 
 function loadorder(order) {
-	let div = document.querySelector('#user');
-	let h2 = document.createElement('h2');
-    h2.innerHTML = "Pedido para: ";
-    let p = document.createElement('p');
-    p.innerHTML = order.user.name + " | " + order.user.email + " | " + order.user.phone;
-    div.appendChild(h2);
-    div.appendChild(p);
-
-    div = document.querySelector('#delivery');
-    h2 = document.createElement('h2');
-    h2.innerHTML = "Entregar em: ";
-    p = document.createElement('p');
-    p.innerHTML = order.delivery.address + ", " + order.delivery.number + " (" + order.delivery.complement + "), " + order.delivery.district + ", " + order.delivery.city + " - " + order
-    .delivery.state, order.delivery.cef;
-    div.appendChild(h2);
-    div.appendChild(p);
-
-    h2 = document.createElement('h2');
-    h2.innerHTML = "Tipo de entrega: ";
-    p = document.createElement('p');
-    p.innerHTML = order.delivery.delivery_method + " (" + order.delivery.time + " dias).";
-    div.appendChild(h2);
-    div.appendChild(p);
-
-    div = document.querySelector('#cart');
-    h2 = document.createElement('h2');
-    h2.innerHTML = "Tipo de pagamento: ";
-    p = document.createElement('p');
-    p.innerHTML = order.payment.payment_method;
-    div.appendChild(h2);
-    div.appendChild(p);
-
+	let p = document.querySelector('#delivery-deadline');
+    p.innerHTML += " (" + order.delivery.time + " dias).";
+    
+    let div = document.querySelector('#cart');
     h2 = document.createElement('h2');
     h2.innerHTML = 'Total: R$ ' + order.payment.total;
     div.appendChild(h2);
@@ -48,5 +20,5 @@ function confirm() {
 
 window.onload = function(e) {
     order = JSON.parse(sessionStorage.getItem('order'));
-    //loadorder(order); //esse exibe pelo javascript
+    loadorder(order); //esse exibe pelo javascript o tempo de entrega e total (que estão fora do form)
 }
